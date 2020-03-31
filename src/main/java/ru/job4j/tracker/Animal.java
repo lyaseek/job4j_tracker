e3cd7b9d958f25632ec417bd23240fb79af7d8c1
lyaseek <lyaseek@gmail.com>
2020-03-31 18:35:59 +0200

    1. Конструктор по умолчанию.[#250191]

--- src/main/java/ru/job4j/tracker/Animal.java
+++ src/main/java/ru/job4j/tracker/Animal.java
@@ -1,29 +1,35 @@
 package ru.job4j.tracker;
 
 public class Animal {
+    String name;
     public Animal() {
         System.out.println("Animal");
     }
 
+    public Animal(String name) {
+        this.name = name;
+        System.out.println("Animal");
+    }
+
     public static class Predator extends Animal {
-        public Predator() {
-            super();
+        public Predator(String name) {
+            super(name);
             System.out.println("Predator");
         }
     }
 
     public static class Tiger extends Predator {
-        public Tiger() {
-            super();
+        public Tiger(String name) {
+            super(name);
             System.out.println("Tiger");
         }
     }
 
     public static void main(String[] args) {
-        Animal animal = new Animal();
+        Animal animal = new Animal("Animal");
         System.out.println();
-        Predator predator = new Predator();
+        Predator predator = new Predator("Predator");
         System.out.println();
-        Tiger tiger = new Tiger();
+        Tiger tiger = new Tiger("Tiger");
     }
 }
