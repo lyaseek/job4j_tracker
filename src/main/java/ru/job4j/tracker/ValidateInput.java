f9c68a85c97aa9c8b9c9d3720f78a6941bee3be3
lyaseek <lyaseek@gmail.com>
2020-04-23 11:03:57 +0200

    1. Обеспечить бесперебойную работу приложения Tracker.[#250165]

---
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -0,0 +1,40 @@
+package ru.job4j.tracker;
+
+public class ValidateInput extends ConsoleInput {
+    @Override
+    public String askStr(String question) {
+        return super.askStr(question);
+    }
+
+    @Override
+    public int askInt(String question) {
+        boolean invalid = true;
+        int value = -1;
+        do {
+            try {
+                value = super.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter validate data again.");
+            }
+        } while (invalid);
+        return value;
+    }
+
+    @Override
+    public int askInt(String question, int max) {
+        boolean invalid = true;
+        int value = -1;
+        do {
+            try {
+                value = super.askInt(question, max);
+                invalid = false;
+            } catch (IllegalStateException moe) {
+                System.out.println("Please select key from menu.");
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter validate data again.");
+            }
+        } while (invalid);
+        return value;
+    }
+}
